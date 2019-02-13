6ffe1aa1ebf4fd6770f10172b3282c708b1aee74
Óscar Martín de la Fuente <omdelafuentes@gmail.com>
2019-02-13 12:47:10 +0100

    First commit

---
+++ src/main/java/com/oscar/cicalc/CiCalcApplication.java
@@ -0,0 +1,14 @@
+package com.oscar.cicalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCalcApplication.class, args);
+    }
+
+}
+
