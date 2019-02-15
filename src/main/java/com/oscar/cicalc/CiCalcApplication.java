1a8b6d059b77c580c1e67227b268ba0b51670803
Óscar Martín de la Fuente <omdelafuentes@gmail.com>
2019-02-15 10:41:24 +0100

    Added Tomcat container and war packaging

--- src/main/java/com/oscar/cicalc/CiCalcApplication.java
+++ src/main/java/com/oscar/cicalc/CiCalcApplication.java
@@ -2,9 +2,10 @@ package com.oscar.cicalc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CiCalcApplication {
+public class CiCalcApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CiCalcApplication.class, args);
